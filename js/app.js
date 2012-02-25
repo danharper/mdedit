ef5758da94ddba19db0c6d42f0baa6580c41129a
Dan Harper <intouch@danharper.me>
2012-02-25 21:14:16 +0000

    BOOM!

---
+++ js/app.js
@@ -0,0 +1,98 @@
+$(function() {
+	var app = window.app || {
+		Router: {},
+		Collections: {},
+		Models: {},
+		Views: {},
+		router: {},
+		collections: {},
+		models: {},
+		views: {}
+	};
+	window.app = app;
+
+	app.Router = Backbone.Router.extend({
+		routes: {
+			'*path': 'home'
+		},
+
+		home: function() {
+			this.views = {}
+
+			this.views.editor = new app.Views.Editor({
+				model: app.models.post
+			});
+			$('#editor-wrap').empty().append(this.views.editor.render().el);
+
+			this.views.viewer = new app.Views.Viewer({
+				model: app.models.post
+			});
+			$('#viewer-wrap').empty().append(this.views.viewer.render().el);
+		}
+	});
+
+	app.Models.Post = Backbone.Model.extend({
+		defaults: {
+			content: ""
+		}
+	});
+
+	app.models.post = new app.Models.Post();
+
+	app.Views.Editor = Backbone.View.extend({
+		events: {
+			'keyup textarea': 'updateContent',
+			'click button': 'dummyData',
+		},
+
+		initialize: function() {
+			var templateSrc;
+			_.bindAll(this, 'render');
+			// this.model.bind('change', this.render);
+			templateSrc = $('#editor-template').html();
+			this.template = Handlebars.compile(templateSrc);
+		},
+
+		render: function() {
+			$(this.el).html(this.template(
+				this.model.toJSON()
+			));
+			return this;
+		},
+
+		updateContent: function() {
+			this.model.set({
+				content: markdown.toHTML($('textarea').val())
+			});
+		},
+
+		dummyData: function() {
+			this.model.set({
+				content: $('#dummy-data').html()
+			});
+			$('textarea').val($('#dummy-data').html()).trigger('keyup');
+		}
+	});
+
+	app.Views.Viewer = Backbone.View.extend({
+		id: 'viewer',
+
+		initialize: function() {
+			var templateSrc;
+			_.bindAll(this, 'render');
+			this.model.bind('change', this.render);
+			templateSrc = $('#viewer-template').html();
+			this.template = Handlebars.compile(templateSrc);
+		},
+
+		render: function() {
+			$(this.el).html(this.template({
+				content: this.model.get('content')
+			}));
+			return this;
+		}
+	});
+
+	app.router = new app.Router();
+	Backbone.history.start({ pushState: true });
+});
